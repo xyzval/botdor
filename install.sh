d346cc4dc7d123c6ffa67eb044037903b2cd5a55
xyzval <r8149762@gmail.com>
2025-06-24 21:38:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# Update sistem dan install dependensi
 apt update && apt install -y python3 python3-pip git
 
-# Clone repository GitHub
+# Clone repo tanpa folder bot
 rm -rf botdor
 git clone https://github.com/xyzval/botdor.git
-cd botdor/bot
+cd botdor
 
-# Install requirements dan jalankan bot
+# Langsung install di root
 pip3 install -r requirements.txt
 nohup python3 bot.py &
-
 echo "Bot berhasil dijalankan di latar belakang."
