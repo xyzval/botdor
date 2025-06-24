7e0c563524c79eab10b2418015153326ce863621
xyzval <r8149762@gmail.com>
2025-06-24 21:16:58 +0700

    Update api.py

--- api.py
+++ api.py
@@ -2,23 +2,23 @@ import requests
 from config import EMAIL, PASSWORD, API_KEY, BASE_URL
 
 def get_token_xl():
-    url = f"{BASE_URL}/loginotp?hesdastore={API_KEY}"
-    return requests.get(url, auth=(EMAIL, PASSWORD)).json()
+    url = f"{BASE_URL}/loginotp?hesdastore={Fv2dN2rKpjSPDDyBaX}"
+    return requests.get(url, auth=(r8149762@gmail.com, Lugoblok@1)).json()
 
 def get_token_session():
-    url = f"{BASE_URL}/sessionotp?hesdastore={API_KEY}"
-    return requests.get(url, auth=(EMAIL, PASSWORD)).json()
+    url = f"{BASE_URL}/sessionotp?hesdastore={Fv2dN2rKpjSPDDyBaX}"
+    return requests.get(url, auth=(r8149762@gmail.com, Lugoblok)).json()
 
 def beli_paket(nomor, paket_id, token):
     url = f"{BASE_URL}/orderotp"
     payload = {
-        "hesdastore": API_KEY,
+        "hesdastore": Fv2dN2rKpjSPDDyBaX,
         "token": token,
         "nomor": nomor,
         "package_id": paket_id
     }
-    return requests.post(url, auth=(EMAIL, PASSWORD), data=payload).json()
+    return requests.post(url, auth=(r8149762@gmail.com, Lugoblok@1), data=payload).json()
 
 def cek_status(order_id):
-    url = f"{BASE_URL}/cekstatusotp?hesdastore={API_KEY}&order_id={order_id}"
-    return requests.get(url, auth=(EMAIL, PASSWORD)).json()
+    url = f"{BASE_URL}/cekstatusotp?hesdastore={Fv2dN2rKpjSPDDyBaX}&order_id={order_id}"
+    return requests.get(url, auth=(r8149762@gmail.com, Lugoblok@1)).json()
