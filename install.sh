635331e8e71615386cf0f6863215470c230c5784
xyzval <r8149762@gmail.com>
2025-06-25 21:27:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ apt update && apt install -y python3 python3-pip git
 rm -rf botdor
 git clone https://github.com/xyzval/botdor.git
 cd botdor || exit
-pip3 install -r requirements.txt --break-system-packages
+pip3 install -r requirements.txt
 nohup python3 bot.py &
 echo "✅ Bot berhasil dijalankan di latar belakang."
